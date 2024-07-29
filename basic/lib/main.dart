b671b1d3bf0caca4b12720903232914850786f36
seon <darkness.blooms.sun@gmail.com>
2024-07-29 15:00:16 +0900

    컬렉션 Set 타입 함수

--- basic/lib/main.dart
+++ basic/lib/main.dart
@@ -4,21 +4,16 @@ void main(){
   //여러값을 하나의 변수에 저장할수 있는 타입
   //서로의 타입으로 자유롭게 형변환이 가능하다는 장점
 
-  //Map 타입
-  // 키와 값의 짝을 저장함
-  // 키를 이용해 원하는 값을 빠르게 찾는데 중점을 둠
-  // Map<키의 타입,값으타입>맵이름
-  Map<String,String> dictionary={
-    'Harry Poter':'해리포터',
-    'Ron Weasley':'론위즐리',
-    'Hermione Granger':'헤르미온느 그레인저',
-  };
-  print(dictionary['HarryPoter']);
-  print(dictionary['Hermione Granger']);
+ // Set타입
+ //  중복 없는 값들의 집합
+  // Set<타입>세트이름
 
-  //키값 반환하기
-  //Iterable가 반환되기에 .toList()사용가능
-  print(dictionary.keys);
-  print(dictionary.values);
+  Set<String> blackPink={'로제','지수','지수','제니','제니'};
+  print(blackPink);
+  print(blackPink.contains('로제'));//값이 있는지 확인 bool값 반환
+  print(blackPink.toList()); //Set-> List변환
+
+  List<String> blackPink2=['로제','지수','제니'];
+  print(Set.from(blackPink2)); // List-> Set 변환
 
 }
