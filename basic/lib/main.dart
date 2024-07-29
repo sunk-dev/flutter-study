77f432d94fe3ec6fcae6ca3cb10ffc8098882e8e
seon <darkness.blooms.sun@gmail.com>
2024-07-29 15:32:34 +0900

    함수의 특징과 매개변수 지정방법

--- basic/lib/main.dart
+++ basic/lib/main.dart
@@ -1,60 +1,58 @@
+int addTwoNumbers(int a, int b){ //위치 매개변수, int a가 intb 보다 먼저 선언됬기에 함수를 사용할때도 1,2 순서대로 a,b에 지정됨.
+  return a+b;
+}
+
+int addTwoNumbersWithNamedParameter({
+  required int a, //NamedParameter 사용 방법
+  required int b,
+}){return a+b;
+}
+
+//기본값을 갖는 포지셔널 파라미터 -> []기호를 사용
+int addTwoNumbersWithDefaultParameter(int a, [int b=2]){
+  return a+b;
+}
+
+int addTwoNumbersWithNamedandDefault({
+  required int a,
+  int b=2,
+}){
+  return a+b;
+}
+
+int addTwoNumbersWithNamedandPositional(
+    int a,{
+  required int b,
+  int c=2,
+}){
+  return a+b+c;
+}
+
+
 void main(){
-  // 연산자
-  // 1. 기본 수치 연산자
-  double number=2;
-  print(number+2);
-  print(number-2);
-  print(number*2);
-  print(number/2);
-  print(number%3);
-  //단항 연산자
-  number++;
-  number--;
-  number+=2;
-  number-=2;
-  number*=2;
-  number/=2;
+// 함수와 람다
+//함수의 일반적 특징
+//한번만 작성하고 여러곳에서 재활용할수 있음
 
-  //null 관련연산자
-  // null:아무겂도 없음
-  // 타입뒤에 ?를 추가해야 null값이 저장될수 있음.
+print(addTwoNumbers(1,3));
+print(addTwoNumbers(5,3));
 
-  double? number1=1;
-  number1=null;
-  print(number1);
+// 매개변수를 지정하는 방법
+//1. Positional Parameter: 순서가 고정된,
+//2. Named Parameter: 이름이 있는 매개변수 required 키워드 사용
 
-  //null를 가잘수 있는 변수에 새로운 값을 추가할때 ??을추가시,
-  // 기존에 null인 때에만 새로운 값이 저장되도록할수 있음
+print(addTwoNumbersWithNamedParameter(a: 2, b: 2));
 
-  double? number2=null;
-  number2 ??=3;
-  print(number2);
+//기본값을 갖는 포지셔널 파라미터 -> []기호를 사용
+print(addTwoNumbersWithDefaultParameter(8));
 
-  number2 ??=4; //이전값이 null이 아니라 3이 유지됨.
-  print(number2);
+//  네임드 파라미터에 기본값 적용시
+print(addTwoNumbersWithNamedandDefault(a: 7));
 
-  //3 값 비교 연산자
-  int number3=1;
-  int number4=2;
-  print(number3>number4);
-  print(number3<number4);
-  print(number3>=number4);
-  print(number3<=number4);
-  print(number3==number4);
-  print(number3!=number4);
+//포지션 파라미터와 네임드 파라터를 섞어쓸때는 포지션이 네임드 파라미터보다 앞에 와야함.
+print(addTwoNumbersWithNamedandPositional(4, b: 8));
 
-  //4. 타입 비교 연삱자
-  print(number3 is int);
-  print(number3 is String);
-  print(number3 is! int);
-  print(number3 is! int);
-  
-  //5. 논리 연산자
-  //&& -> and
-  //|| -> or
-  bool result1 =12>10&& 1>0; // 12가 10보다 크고 1이 0보다 클때
-  bool result2=12>10|| 1<0; // 12가 10보다 크고 1이 0보다 클때
-  print(result1);
-  print(result2);
+
+
 
 }
