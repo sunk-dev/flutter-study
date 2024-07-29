7e913e5d3292b2f398e776652e96bd174a5a34f0
seon <darkness.blooms.sun@gmail.com>
2024-07-29 20:57:32 +0900

    객체지향, 생성자

--- basic/lib/main.dart
+++ basic/lib/main.dart
@@ -1,25 +1,28 @@
 import 'dart:io';
-//클래스
+//생성자
+//인스턴스를 생성하는 메서드
 class Idol{
-  //클래스에 종속되는 변수지정
-  String name='블랙핑크';
-  
-  //클래스에 종속되는 함수=> 메서드
-
+  //생성자에서 입력받는 변수들은 일반적으로 final 키워드 사용
+  final String name;
+  // 생성자 선언
+  // 클래스와 같은 이름이어랴함
+  // 함수의 매개변수를 선언하는 것처럼 매개변수를 지정해줌
+  Idol(String name):this.name=name;
   void sayName(){
-    //클래스 내부의 속성을 지칭하고 싶을때는 this 키워드 사용
     print('저는 ${this.name}입니다');
-    //스코프 안에 같은 속성이름이 하나만 존재한다면 this 생략가능
-    print('저는 $name 입니다');
   }
 }
 
 void main() {
   
   //인스턴스 생성
-  Idol blackPink=Idol();
+  Idol blackPink=Idol('블랙핑크');
   //메서드 실행
   blackPink.sayName();
+
+  //name 에 BTS 저장
+  Idol bts=Idol('BTS');
+  bts.sayName();
   
   
 }
