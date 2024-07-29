a9bbd148c2291f9db3dd6cc6abb81e04490519ef
seon <darkness.blooms.sun@gmail.com>
2024-07-29 15:56:54 +0900

    typedef

--- basic/lib/main.dart
+++ basic/lib/main.dart
@@ -1,23 +1,32 @@
 import 'dart:io';
 
-void main()
-{
-  //익명함수와 람다함수
-  // 둘ㄷ 함수 이름이 없고 일회성으로 사용됨.
-  // 익명함수
-  // (매개변수){함수바디}
-  // 람다함수
-  // (매개변수)=>{함수바디}
-   List<int> numbers=[1,2,3,4,5];
+typedef Operation = void Function(int x, int y);
 
-   //일반함수로 모든값 더하기
-  final allNumbers=numbers.reduce((v,e){return v+e; });
-  print(allNumbers);
+void add(int x, int y) {
+  print('결과값: ${x + y}');
+}
 
-  //람다함수로 모든값 더하기
-  final allNumberswithlamda=numbers.reduce((v,e)=>v+e);
-  print(allNumberswithlamda);
+void subtract(int x, int y) {
+  print('결과값: ${x - y}');
+}
 
+void calculate(int x, int y,Operation oper) {
+ oper(x,y);
+}
 
+void main() {
+//typedef와 함수
+//typedef는 함수의 시그니처(반환값 타입,매개변수 개수와 타입..)를 정의하는 값
 
+//typedef는 일반적인 변수의 type처럼 사용가능
+  Operation oper = add;
+  oper(1, 2);
+
+//subtract() 함수도 Operation에 해당되는
+//시그니처 이므로 oper변수에 저장가능
+  oper = subtract;
+  oper(1, 2);
+
+  // 다트에서 함수는 일급객체라 함수를 값처럼 사용가능
+  calculate(1, 3, add);
 }
