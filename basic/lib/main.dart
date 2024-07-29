2565b697bf8d2182e2529b5eb819d3c3efb04fb9
seon <darkness.blooms.sun@gmail.com>
2024-07-29 20:45:28 +0900

    객체지향, 클래스

--- basic/lib/main.dart
+++ basic/lib/main.dart
@@ -1,26 +1,25 @@
 import 'dart:io';
+//클래스
+class Idol{
+  //클래스에 종속되는 변수지정
+  String name='블랙핑크';
+  
+  //클래스에 종속되는 함수=> 메서드
 
+  void sayName(){
+    //클래스 내부의 속성을 지칭하고 싶을때는 this 키워드 사용
+    print('저는 ${this.name}입니다');
+    //스코프 안에 같은 속성이름이 하나만 존재한다면 this 생략가능
+    print('저는 $name 입니다');
+  }
+}
 
 void main() {
-
-  //try..catch
-  try{
-    //에러가 없을때 실행할 로직
-    final String name='코드팩토리';
-    print(name);
-  }catch(e){
-    //에러가 있을때 실행할로직
-    print(e);
-
-  }
-
-  // throw 키워드-> 고의적으로 에러를 발생시킴
-  try{
-    final name2='코도도';
-    throw Exception('이름이 잘못됬습니다!');
-    print(name2);
-  }catch(e){
-    print(e);
-  }
-
+  
+  //인스턴스 생성
+  Idol blackPink=Idol();
+  //메서드 실행
+  blackPink.sayName();
+  
+  
 }
