dccfb3bc8212ec7793af7f2909333804e0f0582c
seon <darkness.blooms.sun@gmail.com>
2024-07-29 14:14:51 +0900

    컬렉션 List 타입

--- basic/lib/main.dart
+++ basic/lib/main.dart
@@ -1,57 +1,21 @@
 void main(){
-  //var를 사용한 변수 선언
-  //변수명은 중복 불가능
-  var name='코드팩토리';
-  print(name);
 
-  //변수값 변경가능
-  name='퍼플래빗';
-  print(name);
+  // 컬렉션
+  //여러값을 하나의 변수에 저장할수 있는 타입
+  //서로의 타입으로 자유롭게 형변환이 가능하다는 장점
 
-  //dynamic를 사용한 변수 선언
-  //var 타입은 변수의 값을 사용해서 한번 변수의 타입을 유추하면 추론된 타입으로 고정됨
-  //따라서 고정된 변수 타입과 다른 변수타입의 값을 저장하려고 하면 에러 발생
+  //List 여러값을 순서대로 한 변수에 저장할때 사용됨
+  //원소-> 리스트의 구성단위
+  //리스트명[인덱스]
 
-  //but dynamic 키워드 사용시 변수의 타입이 고정되지 않아서 다른 타입의 값 저장가능
-  dynamic newname='코드팩토리';
-  newname=1;
-  
-  //final,const 사용한 변수선언
-  final String idolname='블랙핑크';
-  // idolname='BTS'; => 에러 발생 final로 선언한 변수는 선언후 값 변경 불가
-  
-  const String idolname2='BTS';
-  // idolname2='블랙핑크'; => 에러 발생, const로 선언한 변수는 선언후 값 변경 불가
-  
-  //final const 차이
-  //final : 런타임 상수, 코드가 실행될때 값이 확정되면 사용
-  //const : 빌드타임 상수, 코드를 실행하지 않은 상태에서 값이 확정될떄 사용
+  List <String> blackPinkList=['리사','지수','제니','로제'];
+  print(blackPinkList);
+  print(blackPinkList[0]); //첫원소 지정
+  print(blackPinkList[3]);//마지막 원소 지정
 
+  print(blackPinkList.length);//리스트 길이 반환
 
-  //DateTime.now() => 함수가 실행되는 순간의 날짜를 제공함 즉, 런타임, 실행을 해봐야 값을 알수 있음.
-  final DateTime now=DateTime.now();
-  print(now);
-  
-  //하지만 const 변수를 사용시 빌드타임에 값을 알수 있기때문에 DateTime.now() 함수를 const에 선언하여 사용하면 에러가 발생함
-  // const DateTime errornow= DateTime.now();
-  // print(errornow);
-
-  //1.3 변수 타입
-  //String-문자열
-  String strname='코드팩토리';
-
-  //int -정수
-  int isInt=10;
-
-  //double -실수
-  double isDouble=3.0;
-
-  //bool -불리언
-  bool isTrue=true;
-
-  print(isInt);
-  print(isDouble);
-  print(isTrue);
-
+  blackPinkList[3]='코드팩토리';//인덱스값 변경
+  print(blackPinkList);
 
 }
