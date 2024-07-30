232c65a3ac499c305fa221f1be3e7f0cb182f230
seon <darkness.blooms.sun@gmail.com>
2024-07-30 11:12:26 +0900

    프라이빗 변수

--- basic/lib/main.dart
+++ basic/lib/main.dart
@@ -1,35 +1,19 @@
 import 'dart:io';
-//네임드 생성자
+//프라이빗변수
+//같은 파일에서만 접근 가능한 변수
 
 class Idol {
-  final String name;
-  final int membersCount;
-
-  Idol(String name, int membersCount)
-      //1개 이상의 변수를 저장하고 싶을때는 기호로 연결해주면 됨.
-      : this.name = name,
-        this.membersCount = membersCount;
-
-  //네임드 생성자
-//{클래스명.네임드 생성자명} 형식
-//나머지 과정은 기본 생성자와 같음
-  Idol.fromMap(Map<String, dynamic> map)
-      : this.name = map['name'],
-        this.membersCount = map['membersCount'];
-  void sayName(){
-    print('저는 ${this.name}입니다. ${this.name} 멤버는'
-        '${this.membersCount}명 입니다 ');
-
-  }
+  // '_'로 변수명을 시작하면
+  // 프라이빗 변수를 선언할수 있음
+  String _name;
+  Idol(this._name);
 }
 
 void main() {
+  Idol blackPink=Idol('블랙핑크');
 
-  //기본 생성자 사용
-  Idol blackPink=Idol('블랙핑크', 4);
-  blackPink.sayName();
+  //같은 파일에서는  _name 변수에 접근할수 있지만
+  // 다른 파일에서는 접근 불가
+  print(blackPink._name);
 
-  //formMap이라는 네임드 생성자사용
-  Idol bts=Idol.fromMap({'name':'BTS','membersCount':7,} );
-  bts.sayName();
 }
