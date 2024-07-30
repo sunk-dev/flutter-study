98ecd24a95577c29c2b7289ddb6474cc1339f122
seon <darkness.blooms.sun@gmail.com>
2024-07-30 14:36:57 +0900

    Stream(1) -> 브로드 캐스트 스트림

--- asynchronous_programming/lib/main.dart
+++ asynchronous_programming/lib/main.dart
@@ -3,23 +3,34 @@
 //Stream -> 지속적으로 값을 반환 받을때 사용
 // Stream 은 한번 리슨하면 Stream에 주입되는 모든 값들을 지속적으로 받아옴.
 
-// 1. Stream 기본 사용법
-// stream 이용시 dart:async 패키지 이용
+// 2. 브로드캐스트 스트림
+// stream은 단한번만 listen을 항수 있음
+//브로드 캐스드 스트림을 사용하면 Stream을 여러번 listen하도록 반환
 import 'dart:async';
 
 void main() async{
   final controller=StreamController();// StreamController 선언
-  final stream=controller.stream;// Stream 가져오기
 
-  //Stream listen() 함수를 실행하면 값이 주입될때마다 콜백함수를 실행가능
-  final streamListener1= stream.listen((val){print(val);});
+  //여러번 리슨 할수 있는 Broadcast Stream 객체 생성
+  final stream=controller.stream.asBroadcastStream();//
+
+  //첫 listen()함수
+  final streamListener1=stream.listen((val){
+    print('listen1');
+    print(val);
+  });
+
+  //두번째 listen()함수
+  final streamListener2=stream.listen((val){
+    print('listen2');
+    print(val);
+  });
 
 
-  //Stream에 값 주입
+  //add() 실행할때마다 listen()하는 모든 콜백함수에 값이 주입됨.
   controller.sink.add(1);
   controller.sink.add(2);
   controller.sink.add(3);
-  controller.sink.add(4);
 
 
 }
