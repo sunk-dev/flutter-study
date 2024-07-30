4c76db2707e8f132bef77371d86af26924201e6f
seon <darkness.blooms.sun@gmail.com>
2024-07-30 13:32:14 +0900

    제네릭 ★

--- basic/lib/main.dart
+++ basic/lib/main.dart
@@ -1,47 +1,22 @@
 import 'dart:io';
 
-//추상
-//상속이나 인터페이스로 사용하는데 칠요한 속성만 정의하고 인스턴스화 할수도 없도록하는 기능
-//추상 클래스는 추상메서드를 선언할수 있으며 추상메서드는 함수의 반환타입, 이름 매개변수만을 정의하고
-//함수의 바디의 선언을 자식클래스에서 필수로 정의하도록 강제함.
+//제네릭 generic
+//특정변수의 타이ㅂ을 하나의 타입으로 제한하고 싶지 않을때 자주 사용함.
+//<> 사이에 입력되는 값이 제네릭 문자
 
-abstract class Idol {
-  final String name;
-  final int membersCount;
+//인스턴스화 할때 입력받을 타입을 T로 지정함.
+class Cache<T> {
+  //data의 타입을 추후 입력될 T타입으로 지정함.
+  final T data;
 
-  Idol(this.name, this.membersCount);
-
-  void sayName() ;
-
-  void sayMembersCount() ;
-}
-
-class GirlGroup implements Idol{
-  final String name;
-  final int membersCount;
-
-  GirlGroup(this.name,this.membersCount);
-
-
-  @override
-  void sayMembersCount() {
-    // TODO: implement sayMembersCount
-    print('${this.name}의 멤버는 ${this.membersCount}명 입니다');
-  }
-
-  @override
-  void sayName() {
-    // TODO: implement sayName
-    print('저는 여자아이돌 ${this.name}입니다');
-  }
+  Cache({required this.data});
 
 }
-
 
 void main() {
+  //T의 타입을 List<int>로 입력
+  final cache=Cache<List<int>>(data: [1,2,3]);
 
-  GirlGroup blackPink=GirlGroup('블랙핑크', 4);
-  blackPink.sayName();
-  blackPink.sayMembersCount();
-
+  //제네릭에 입력된 값을 통해 data 변수의 타입이 자동으로 유추됨
+  print(cache.data.reduce((v,e)=>v+e));
 }
