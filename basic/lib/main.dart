ec3efe0ec2e0f86cb9f4cbff70bdc1bb0047748a
seon <darkness.blooms.sun@gmail.com>
2024-07-30 11:19:17 +0900

    게터와 세터

--- basic/lib/main.dart
+++ basic/lib/main.dart
@@ -1,19 +1,31 @@
 import 'dart:io';
-//프라이빗변수
-//같은 파일에서만 접근 가능한 변수
 
+//게터세터
+//게터: 값을 가져올때
+//세터: 값을 지정할때
+//사용시 모두 변수처럼 사용한다. 즉 사용할때 메서드명뒤에 ()를 붙이지 않음.
+
+//세터는 거의 사용하지 않지만 게터는 종종 사용함
 class Idol {
   // '_'로 변수명을 시작하면
   // 프라이빗 변수를 선언할수 있음
-  String _name;
-  Idol(this._name);
+  String _name = '블랙핑크';
+
+  //get 키워드를 사용해서 게터임을 명시
+  //게터는 메서드와 다르게 매개변수를 전혀 받지 않는다.
+  String get name {
+    return this._name;
+  }
+
+//세터는 set이라는 키워드를 사용해서 선언
+//세터는 매개변수로 딱하나의 변수를 받을수 있음
+  set name(String name){
+    this._name=name;
+  }
 }
 
 void main() {
-  Idol blackPink=Idol('블랙핑크');
-
-  //같은 파일에서는  _name 변수에 접근할수 있지만
-  // 다른 파일에서는 접근 불가
-  print(blackPink._name);
-
+  Idol blackPink = Idol();
+  blackPink.name='에이핑크';
+  print(blackPink.name);
 }
