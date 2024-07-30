7988e2b574bc99ecfc4d195c80b39ff779aba6f2
seon <darkness.blooms.sun@gmail.com>
2024-07-30 11:08:17 +0900

    0730-> 네임드 생성자

--- basic/lib/main.dart
+++ basic/lib/main.dart
@@ -1,30 +1,35 @@
 import 'dart:io';
-//생성자
-//인스턴스를 생성하는 메서드
-class Idol{
-  //생성자에서 입력받는 변수들은 일반적으로 final 키워드 사용
-  final String name;
-  // 생성자 선언
-  // 클래스와 같은 이름이어랴함
-  // 함수의 매개변수를 선언하는 것처럼 매개변수를 지정해줌
+//네임드 생성자
 
-  //변수 저장과정을 생략하는 버전
-  Idol(this.name);
+class Idol {
+  final String name;
+  final int membersCount;
+
+  Idol(String name, int membersCount)
+      //1개 이상의 변수를 저장하고 싶을때는 기호로 연결해주면 됨.
+      : this.name = name,
+        this.membersCount = membersCount;
+
+  //네임드 생성자
+//{클래스명.네임드 생성자명} 형식
+//나머지 과정은 기본 생성자와 같음
+  Idol.fromMap(Map<String, dynamic> map)
+      : this.name = map['name'],
+        this.membersCount = map['membersCount'];
   void sayName(){
-    print('저는 ${this.name}입니다');
+    print('저는 ${this.name}입니다. ${this.name} 멤버는'
+        '${this.membersCount}명 입니다 ');
+
   }
 }
 
 void main() {
-  
-  //인스턴스 생성
-  Idol blackPink=Idol('블랙핑크');
-  //메서드 실행
+
+  //기본 생성자 사용
+  Idol blackPink=Idol('블랙핑크', 4);
   blackPink.sayName();
 
-  //name 에 BTS 저장
-  Idol bts=Idol('BTS');
+  //formMap이라는 네임드 생성자사용
+  Idol bts=Idol.fromMap({'name':'BTS','membersCount':7,} );
   bts.sayName();
-  
-  
 }
