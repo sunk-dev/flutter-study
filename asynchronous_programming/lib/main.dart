ba4edf429422875319e7c228daf8850f8309dd2f
seon <darkness.blooms.sun@gmail.com>
2024-07-30 14:26:37 +0900

    async와 await (2)-> 결과값 반환받기

--- asynchronous_programming/lib/main.dart
+++ asynchronous_programming/lib/main.dart
@@ -4,7 +4,8 @@
 //비동기 프로그래밍 특징을 유지하면서 코드가 작성된 순서대로 프로그램을 실해ㅇ힘
 
 //async 키워드는 함수 매개변수 정의와 바디사이에 입력함
-Future<void> addNumbers(int number1,int number2) async{
+//결과값 반환가능!
+Future<int> addNumbers(int number1,int number2) async{
   print('$number1+$number2 계산 시작!');
 
   //await는 대기하고 싶은 비동기 함수 앞에 입력
@@ -13,12 +14,15 @@ Future<void> addNumbers(int number1,int number2) async{
   });
 
   print('$number1+$number2 코드실행끝!');
+  return number1+number2;
 }
 
 void main() async{
 
-  await addNumbers(1,1); //addNumbers 함수가 순차적으로 실행되길 원할때
-  await addNumbers(2,2);
+  final result1=await addNumbers(1,1); //addNumbers 함수가 순차적으로 실행되길 원할때
+  print('결과값 $result1');
+  final result2=await addNumbers(2,2);
+  print('결과값 $result2');
 
 
 
