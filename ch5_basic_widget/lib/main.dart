1213fea560205c541ca565117c6bc0b4d0aff8ff
seon <darkness.blooms.sun@gmail.com>
2024-07-31 20:14:01 +0900

    ch5 위젯종류- Container 위젯

--- ch5_basic_widget/lib/main.dart
+++ ch5_basic_widget/lib/main.dart
@@ -1,23 +1,13 @@
 import 'package:flutter/material.dart';
 
 /*
-위젯소개
-  자식들을 하나만 갖는 위젯
-  - 주로 child 매개변수를 입력받음
-  - Container 위젯: 자식을 담는 컨테이너 역할, 단순하게 자식을 담는 역할이 아닌, 배경색, 너비와 높이 ,테두리 등의 디자인
-  - GestureDetecor 위젯: 플러터에서 제공하는 제스터 기능을 자식위젯에서 인식하는 위젯, 탭 드래그 더블클릭같은 제스터 기능이 자식위젯에 인식됬을때 함수 실행
-  - SizedBox 높이와 너비를 지정하는 위젯, Container 위젯과 다르게 디자인적 요소는 적용할수 없고, const 생성자로 선언할수 있어 퍼포먼스 측면에서 효율적
-자식을 여러개갖는 위젯
-  - childern 매개변수를 입력받음
-  - 리스트로 여러 위젯을 입력받음
-  - Column위젯: childern 매개변수에 입력된 모든 위젯들을 세로로 배치
-  - Row 위젯: childern 매개변수에 입력된 모든 위젯들을 가로로 배치
-  - ListView 위젯: 리스트 구현시사용 다수의 위젯 입력이 가능하녀, 입력된 위젯이 화면을 벗어나면 스크롤 가능
+디자인 관련 위젯
+배경을 추가하거나 간격을 추가하거나등 디자인적 요소를 적용할때 사용
+Container 위젯
+- 말 그대로 다른 위젯을 담는데 사용, 위젯의 너비와 높이를 지정하거나, 배경이나 테투리를 추가할때 많이 사용
  */
 
-/*
-FloatingActionButton -> 디자인 형태중하나 플로팅작업 버튼 만들때 사용
- */
+
 
 void main() {
   runApp(FloatingActionButtonExample());
@@ -28,12 +18,28 @@ class FloatingActionButtonExample extends StatelessWidget{
   Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          //클릭했을때 실행할 함수
-          onPressed: (){},
-          child: Text('클릭'),
+        body: Container(
+          //스타일 작용
+          decoration: BoxDecoration(
+            //배경색 적용
+            color: Colors.deepPurple,
+            //테두리적용
+            border: Border.all(
+              //테두리 굵기
+              width: 16.0,
+              //테두리 색상
+              color: Colors.black
+            ),
+            //모서리 둥굴게 만들기
+            borderRadius: BorderRadius.circular(16.0,),
+
+          ),
+          //높이
+          height: 200.0,
+          //너비
+          width: 100.0,
         ),
-        body: Container(),
+
       ),
     );
   }
