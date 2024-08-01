2bb0baf338d13d90715ab700a00b4c20f22124db
seon <darkness.blooms.sun@gmail.com>
2024-08-01 17:11:06 +0900

    ch07 project 블로그웹앱-> 기본 설정

---
+++ blog_web_app/lib/screen/home_screen.dart
@@ -0,0 +1,20 @@
+
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget{
+
+  //const 생성자
+  const HomeScreen({Key?key}):super(key:key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Home Screen'),
+    );
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
