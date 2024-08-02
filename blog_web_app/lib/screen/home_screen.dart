c6d38495cdd3a43641f8a89fea0e7c46426bfda2
seon <darkness.blooms.sun@gmail.com>
2024-08-02 15:27:40 +0900

    2024.08.02 ch07 project 블로그웹앱-> 웹뷰 구현하기

--- blog_web_app/lib/screen/home_screen.dart
+++ blog_web_app/lib/screen/home_screen.dart
@@ -3,6 +3,8 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+//웹뷰플러그인 임포트
+import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget{
 
@@ -11,7 +13,17 @@ class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      appBar: AppBar(
+        //앱바 위젯 추가
+        backgroundColor: Colors.orange,
+        //앱 타이틀 설정
+        title: Text('code Factory'),
+        centerTitle: true,
+      ),
+      body: WebView(
+        initialUrl: 'https://blog.codefactory.ai',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
     );
     // TODO: implement build
     throw UnimplementedError();
