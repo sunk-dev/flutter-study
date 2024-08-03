26e06a917ca9e81e568ab9bc9f20d9e50607f2e7
seon <darkness.blooms.sun@gmail.com>
2024-08-03 16:11:30 +0900

    2024.08.03-> ch08 3. 프로젝트 초기화 하기

---
+++ image_carousel/lib/screen/home_screen.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget{
+  const HomeScreen({Key? key}):super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Text('Home Screen'),
+    );
+    throw UnimplementedError();
+  }
+
+}
