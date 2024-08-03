e2469eea047b37c0574381eb510f591f12235557
seon <darkness.blooms.sun@gmail.com>
2024-08-03 16:30:30 +0900

    2024.08.03-> ch08 4.3. 타이머 추가하기

--- image_carousel/lib/screen/home_screen.dart
+++ image_carousel/lib/screen/home_screen.dart
@@ -2,10 +2,39 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:async';
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatefulWidget{
   const HomeScreen({Key? key}):super(key: key);
+  State<StatefulWidget> createState()=> _HomeScreenState();
 
+}
+
+class _HomeScreenState extends State<HomeScreen>{
+  //PageController 생성
+  final PageController pageController=PageController();
+  @override
+  void initState(){
+    super.initState();
+    Timer.periodic(
+      Duration(seconds: 3),
+      (timer){
+        int? nextPage=pageController.page?.toInt();
+        if(nextPage==null){
+          return;
+        }
+        if(nextPage==4){
+          nextPage=0;
+        }else{
+          nextPage++;
+        }
+        pageController.animateToPage(nextPage,
+            duration: Duration(milliseconds: 500),
+            curve: Curves.ease,
+        );
+        },
+    );
+  }
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -15,9 +44,10 @@ class HomeScreen extends StatelessWidget{
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
     return Scaffold(
       body: PageView(//페이지 뷰 추가
+        controller: pageController,
         children: [1,2,3,4,5]// 샘플리스트 생성
             .map(//위젯으로 매핑
-            (number)=> Image.asset('asset/img/image_$number.jpeg',
+              (number)=> Image.asset('asset/img/image_$number.jpeg',
             fit: BoxFit.cover,),
         ).toList(),
       ),
