2ca7f35c8626e1d2da760a5f54538cfd6ce7caac
seon <darkness.blooms.sun@gmail.com>
2024-08-06 12:58:29 +0900

    chapter10 -> 상수추가하기

---
+++ random_dice/lib/const/colors.dart
@@ -0,0 +1,10 @@
+//반복적으로 사용하는 상수들을 정리하기 위한 별도의 폴더 const를 만듬
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+const backgroungColor=Color(0xFF0E0E0E);
+const primaryColot=Colors.white;
+final secondaryColor=Colors.grey[600]; //const 선언이 가능하지만 600이라는 키값을 입력하면 런타임에 색상이 계산되서 const사용 불가
+
