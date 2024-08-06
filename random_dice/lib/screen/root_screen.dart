4f2205d0c9c20f07870f583c9e5dd607eedbd3f9
seon <darkness.blooms.sun@gmail.com>
2024-08-06 15:11:57 +0900

    chapter10 -> RootScreen 위젯 구현 2

--- random_dice/lib/screen/root_screen.dart
+++ random_dice/lib/screen/root_screen.dart
@@ -2,14 +2,36 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class RootScreen extends StatelessWidget{
+class RootScreen extends StatefulWidget{
   const RootScreen ({Key? key}):super(key: key);
+
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    _RootScreenState();
+    throw UnimplementedError();
+  }
+
+
+}
+
+class _RootScreenState extends State<RootScreen> with
+TickerProviderStateMixin{ //TickerProviderStateMixin 사용하기
+  TabController? controller; //사용할  TabController 선언
+
+  @override
+  void initState(){
+    super.initState();
+    controller=TabController(length: 2, vsync: this); //컨트롤러 초기화
+  }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       body: TabBarView( //탭화면을 보여줄 위젯
+        controller: controller, //컨트롤러 등록
         children: renderChildren(),
       ),
       //아 탭 네비게이션을 구현하는 매개변수
@@ -26,6 +48,4 @@ class RootScreen extends StatelessWidget{
     //탭 네비게이션을 구현하는 위젯
     return BottomNavigationBar(items: []);
   }
-
-
 }
