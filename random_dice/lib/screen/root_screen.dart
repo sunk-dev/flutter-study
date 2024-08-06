864e8b1f8c7bb4e0a875ba471d55cd857d589cc9
seon <darkness.blooms.sun@gmail.com>
2024-08-06 15:28:15 +0900

    chapter10 -> RootScreen 위젯 구현 4 : renderChildren

--- random_dice/lib/screen/root_screen.dart
+++ random_dice/lib/screen/root_screen.dart
@@ -7,11 +7,7 @@ class RootScreen extends StatefulWidget{
 
 
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    _RootScreenState();
-    throw UnimplementedError();
-  }
+  State<RootScreen> createState()=> _RootScreenState();
 
 
 }
@@ -24,7 +20,23 @@ TickerProviderStateMixin{ //TickerProviderStateMixin 사용하기
   void initState(){
     super.initState();
     controller=TabController(length: 2, vsync: this); //컨트롤러 초기화
+
+    // 컨트롤러 속성이 변경 될때마다 실행할 함수 등록
+    controller!.addListener(tabListener);
   }
+
+  tabListener(){ //리스너로 사용할수 있는 함수
+    setState(() {
+
+    });
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller!.removeListener(tabListener); //리스너에 등록한 함수 취소
+    super.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -68,6 +80,13 @@ TickerProviderStateMixin{ //TickerProviderStateMixin 사용하기
   BottomNavigationBar renderBottomNavigation(){
     //탭 네비게이션을 구현하는 위젯
     return BottomNavigationBar(
+      //현재 화면에 랜더링되는 탭의 인덱스
+      currentIndex: controller!.index,
+        onTap: (int index){
+        setState(() {
+          controller!.animateTo(index);
+        });
+        },
         items: [
           BottomNavigationBarItem( //하단 탭바의 각 버튼 구현
               icon: Icon(
