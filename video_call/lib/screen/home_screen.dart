8fa579ebd670a9dbe202925434ab18a3ef35b629
seon <darkness.blooms.sun@gmail.com>
2024-08-08 15:26:14 +0900

    chapter 12 -> 홈스크린 위젯 구현하기

--- video_call/lib/screen/home_screen.dart
+++ video_call/lib/screen/home_screen.dart
@@ -1,17 +1,108 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget{
-  const HomeScreen({Key? key}):super(key: key);
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+        backgroundColor: Colors.blue[100]!,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Expanded(child: _Logo()), //로고
+                Expanded(child: _Image()), //중앙 이미지
+                Expanded(child: _EntryButton()), //화상통화
+              ],
+            ),
+          ),
+        ));
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _Logo extends StatelessWidget {
+  const _Logo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.circular(16.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.blue[300]!,
+              blurRadius: 12.0,
+              spreadRadius: 2.0,
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisSize: MainAxisSize.min, //주축 최소크기
+            children: [
+              Icon(
+                Icons.videocam,
+                color: Colors.white,
+                size: 40.0,
+              ),
+              SizedBox(
+                width: 12.0,
+              ),
+              Text(
+                'LIVE',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30.0,
+                  letterSpacing: 4.0,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
     // TODO: implement build
     throw UnimplementedError();
   }
+}
 
+class _Image extends StatelessWidget {
+  const _Image({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset('asset/img/home_img.png'),
+    );
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _EntryButton extends StatelessWidget {
+  const _EntryButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        ElevatedButton(
+          onPressed: () {},
+          child: Text('입장하기'),
+        ),
+      ],
+    );
+    // TODO: implement build
+    throw UnimplementedError();
+  }
 }
