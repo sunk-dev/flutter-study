789d017f22dbf2eaeecfc3e2036d7743cf08de7b
seon <darkness.blooms.sun@gmail.com>
2024-08-13 14:31:07 +0900

    ch13 -> 오류 해결 (https://darkness-blooms-sun.tistory.com/33)

--- chool_check/lib/screen/home_screen.dart
+++ chool_check/lib/screen/home_screen.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatelessWidget{
+  static final LatLng companyLatLng=LatLng(
+      37.5233273,
+      126.921252,
+  );
   const HomeScreen({Key? key}):super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: renderAppBar(),
-      body: Text('home Screen'),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: companyLatLng,
+          zoom: 16,
+        ),
+      ),
     );
     // TODO: implement build
     throw UnimplementedError();
