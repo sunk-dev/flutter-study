e64ee0ddebf0d86cab054224594676b475fec93a
seon <darkness.blooms.sun@gmail.com>
2024-08-14 11:12:41 +0900

    ch14 AppBar 구현하기

--- image_editpr/lib/screen/home_screen.dart
+++ image_editpr/lib/screen/home_screen.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_editpr/component/main_app_bar.dart';
 
 class HomeScreen extends StatelessWidget{
   const HomeScreen ({Key? key}):super(key: key);
@@ -8,10 +9,30 @@ class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('home_screen'),
+      body: Stack( //스크린에 Body, Appbar, Footer 순서대로 쌓을 준비
+        fit:  StackFit.expand,//자식 위젯들 최대 크기로 펼치기
+        children: [
+          //MainAppBar 좌우 위끝에 정렬
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: MainAppBar(
+              onPickImage: onPickImage,
+              onSaveImage: onSaveImage,
+              onDeleteItem: onDeleteImage,
+            ),
+          ),
+        ],
+
+      ),
     );
     // TODO: implement build
     throw UnimplementedError();
   }
+
+  void onPickImage(){}
+  void onSaveImage(){}
+  void onDeleteImage(){}
 
 }
