d2bd2101c4c3d7ede91e0dce29ad6997dcadca04
seon <darkness.blooms.sun@gmail.com>
2024-08-18 14:37:27 +0900

    ch15 -> ListView 구현하기

--- cf_tube/lib/screen/home_screen.dart
+++ cf_tube/lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cf_tube/component/custom_youtube_player.dart';
 import 'package:cf_tube/model/video_model.dart';
+import 'package:cf_tube/repository/youtube_repository.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -10,11 +11,34 @@ class HomeScreen extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: CustomYoutubePlayer(
-        videoModel: VideoModel(
-          id: '3Ck42C2ZCb8',
-          title: '다트언어 기본기 1시간 만에 끝내기'
+      appBar: AppBar(
+        centerTitle:true,
+        title: Text(
+          '코팩튜브',
         ),
+        backgroundColor: Colors.black,
+      ),
+      body: FutureBuilder<List<VideoModel>>(
+        future: YoutubeRepository.getVideos(),
+        builder: (context,snapshot){
+          if(snapshot.hasError){
+            return Center(
+              child: Text(
+                snapshot.error.toString(),
+              ),
+            );
+          }
+          if(!snapshot.hasData){
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView(
+            physics: BouncingScrollPhysics(),
+            children: snapshot.data!
+            .map((e)=>CustomYoutubePlayer(videoModel: e)).toList(),
+          );
+        },
       ),
     );
     // TODO: implement build
