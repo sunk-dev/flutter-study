6d7921cb913e3bf501f1b12938037d1aec32db45
seon <darkness.blooms.sun@gmail.com>
2024-08-18 15:05:34 +0900

    ch15 -> API 숨김

--- cf_tube/lib/const/api.dart
+++ cf_tube/lib/const/api.dart
@@ -1,3 +1,3 @@
-const API_KEY='AIzaSyDNtM0TCOipNmyuAr_63ZUDEBu1ljDGRZk';
+const API_KEY='내 API 주소';
 const YOUTUBE_API_BASE_URL='https://www.googleapis.com/youtube/v3/search';
 const CF_CHANNEL_ID='UCxZ2AlaT0hOmxzZVbF_j_Sw';
