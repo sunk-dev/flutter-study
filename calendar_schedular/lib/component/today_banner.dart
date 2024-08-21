c8e1ff06feeea863f9f0b7676b8f9412e8be509a
seon <darkness.blooms.sun@gmail.com>
2024-08-21 15:55:24 +0900

    ch16 선택된 날의 일정 보여주기

---
+++ calendar_schedular/lib/component/today_banner.dart
@@ -0,0 +1,43 @@
+import 'package:calendar_schedular/const/colors.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TodayBanner extends StatelessWidget {
+  final DateTime selectedDate;
+  final int count;
+
+  const TodayBanner({
+    required this.selectedDate,
+    required this.count,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+      fontWeight: FontWeight.w600,
+      color: Colors.white,
+    );
+    return Container(
+      color: PRIMARY_COLOR,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              '${selectedDate.year}년 ${selectedDate.month}월 ${selectedDate.day}일',
+              style: textStyle,
+            ),
+            Text(
+              '$count개',
+              style: textStyle,
+            ),
+          ],
+        ),
+      ),
+    );
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
--- calendar_schedular/lib/screen/home_screen.dart
+++ calendar_schedular/lib/screen/home_screen.dart
@@ -1,20 +1,18 @@
 import 'package:calendar_schedular/component/main_calendar.dart';
 import 'package:calendar_schedular/component/schedule_card.dart';
+import 'package:calendar_schedular/component/today_banner.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget{
-  const HomeScreen({Key? key}):super(key: key);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
   @override
   State<HomeScreen> createState() => _HomeScreenState();
-
-
-
-
-
 }
-class _HomeScreenState extends State<HomeScreen>{
-  DateTime selectedDate= DateTime.utc(
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.utc(
     DateTime.now().year,
     DateTime.now().month,
     DateTime.now().day,
@@ -30,6 +28,13 @@ class _HomeScreenState extends State<HomeScreen>{
               selectedDate: selectedDate,
               onDaySelected: onDayselected,
             ),
+            SizedBox(
+              height: 8.0,
+            ),
+            TodayBanner(selectedDate: selectedDate, count: 0),
+            SizedBox(
+              height: 8.0,
+            ),
             ScheduleCard(startTime: 12, endTime: 14, content: '프로그래밍 공부'),
           ],
         ),
@@ -39,10 +44,9 @@ class _HomeScreenState extends State<HomeScreen>{
     throw UnimplementedError();
   }
 
-  void onDayselected(DateTime selectedDate,DateTime foucusedDate){
+  void onDayselected(DateTime selectedDate, DateTime foucusedDate) {
     setState(() {
-      this.selectedDate=selectedDate;
+      this.selectedDate = selectedDate;
     });
   }
-
 }
