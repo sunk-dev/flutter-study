d7d8581b5564268c47fbd6b7ebb821bb92db2cf2
seon <darkness.blooms.sun@gmail.com>
2024-08-21 16:52:50 +0900

    ch16 달력언어 설정하기

--- calendar_schedular/lib/main.dart
+++ calendar_schedular/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:calendar_schedular/screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+void main() async{
+  //플러터 프레임 워크가 준비 될때까지 대기,
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDateFormatting(); //init 패키지 초기화
   runApp(
     MaterialApp(
       home: HomeScreen(),
