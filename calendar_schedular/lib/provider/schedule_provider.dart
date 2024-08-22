068a2bcd4975d296dfa2ff55b459307dda1ddb06
seon <darkness.blooms.sun@gmail.com>
2024-08-22 10:42:10 +0900

    ch18  API  글로벌 상태 관리 구현하기 ScheduleProvider

---
+++ calendar_schedular/lib/provider/schedule_provider.dart
@@ -0,0 +1,72 @@
+import 'package:calendar_schedular/model/schedule_model.dart';
+import 'package:calendar_schedular/repository/schedule_repository.dart';
+import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ScheduleProvider extends ChangeNotifier {
+  final ScheduleRepository repository;
+
+  DateTime selectedDate = DateTime.utc(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+  Map<DateTime, List<ScheduleModel>> cache = {};
+
+  ScheduleProvider({
+    required this.repository,
+  }) : super() {
+    getSchedules(date: selectedDate);
+  }
+
+  void getSchedules({
+    required DateTime date,
+  }) async {
+    final resp = await repository.getSchedules(date: date);
+
+    cache.update(date, (val) => resp, ifAbsent: () => resp);
+    notifyListeners();
+  }
+
+  void createSchedule({
+    required ScheduleModel schedule,
+  }) async {
+    final targetDate = schedule.date;
+    final savedSchedule = await repository.createSchedule(schedule: schedule);
+
+    cache.update(
+      targetDate,
+      (value) => [
+        ...value,
+        schedule.copyWith(
+          id: savedSchedule,
+        ),
+      ]..sort(
+          (a, b) => a.startTime.compareTo(b.startTime),
+        ),
+      ifAbsent: () => [schedule],
+    );
+    notifyListeners();
+  }
+
+  void deleteSchedule({
+    required DateTime date,
+    required String id,
+  }) async {
+    final resp = await repository.deleteSchedule(id: id);
+
+    cache.update(
+      date,
+      (value) => value.where((e) => e.id != id).toList(),
+      ifAbsent: () => [],
+    );
+    notifyListeners();
+  }
+  void changeSelectedDate({
+    required DateTime date,
+}){
+    selectedDate=date;
+    notifyListeners();
+  }
+}
