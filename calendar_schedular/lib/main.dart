7c4947ea527a0733bd59685a2e7ae1ecc5f88adf
seon <darkness.blooms.sun@gmail.com>
2024-08-22 10:44:58 +0900

    ch18  프로바이더 초기화 하기

--- calendar_schedular/lib/main.dart
+++ calendar_schedular/lib/main.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:calendar_schedular/database/drift_database.dart';
 import 'package:get_it/get_it.dart';
+import 'package:calendar_schedular/provider/schedule_provider.dart';
+import 'package:calendar_schedular/repository/schedule_repository.dart';
+import 'package:provider/provider.dart';
 
 void main() async{
   //플러터 프레임 워크가 준비 될때까지 대기,
@@ -12,10 +15,15 @@ void main() async{
   await initializeDateFormatting(); //init 패키지 초기화
   final database=LocalDataBase();
   GetIt.I.registerSingleton<LocalDataBase>(database);
+  final repository=ScheduleRepository();
+  final schdeuleProvider=ScheduleProvider(repository: repository);
   runApp(
-    MaterialApp(
-      home: HomeScreen(),
-    )
+    ChangeNotifierProvider(
+        create: (_)=>schdeuleProvider,
+      child: MaterialApp(
+        home: HomeScreen(),
+      ),
+    ),
   );
 }
 
