9d4cf160d697dfe492b332fc05887e0eafde30bd
seon <darkness.blooms.sun@gmail.com>
2024-08-23 19:07:00 +0900

    ch19 파이어베이스 설정 설정하기

--- calendar_schedular/lib/main.dart
+++ calendar_schedular/lib/main.dart
@@ -8,9 +8,17 @@ import 'package:calendar_schedular/provider/schedule_provider.dart';
 import 'package:calendar_schedular/repository/schedule_repository.dart';
 import 'package:provider/provider.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:calendar_schedular/firebase_options.dart';
+
 void main() async{
   //플러터 프레임 워크가 준비 될때까지 대기,
   WidgetsFlutterBinding.ensureInitialized();
+
+  //파이어 베이스 프로젝트 설정함수
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await initializeDateFormatting(); //init 패키지 초기화
   final database=LocalDataBase();
